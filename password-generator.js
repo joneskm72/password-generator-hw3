c4a9a58d9c102d6af5a420638c4685e641fb2484
Kasey Jones <joneskm72@gmail.com>
2020-07-08 23:01:24 -0400

    Created JS function and button onclick

--- password-generator.js
+++ password-generator.js
@@ -3,20 +3,22 @@ var minLength = 8;
 var maxLength = 128;
 var specialCharacters = " !#$%&'()*+,-./:;<=>?@[]^_{|}~"
 
-var length = prompt("How many characters?");
+function generatePassword() {
+    var length = prompt("How many characters?");
 
-while(length < 8 || length >= 128) {
-    length = prompt("Password length must be between 8 and 128 characters. How many characters?");
-}
-    var lower = confirm("Would you like lowercase characters?");
-    var upper = confirm("Would you like uppercase characters?");
-    var special = confirm("Would you like special characters?");
-    var numbers = confirm("Would you like numerical characters?");
+    while(length < 8 || length >= 128) {
+        length = prompt("Password length must be between 8 and 128 characters. How many characters?");
+    }
+        var lower = confirm("Would you like lowercase characters?");
+        var upper = confirm("Would you like uppercase characters?");
+        var special = confirm("Would you like special characters?");
+        var numbers = confirm("Would you like numerical characters?");
 
-while(lower != true && upper != true && special != true) {
-    alert("Please select at least one character type.");
-    var lower = confirm("Would you like lowercase characters?");
-    var upper = confirm("Would you like uppercase characters?");
-    var special = confirm("Would you like special characters?");
-    var numbers = confirm("Would you like numerical characters?");
+    while(lower != true && upper != true && special != true) {
+        alert("Please select at least one character type.");
+        var lower = confirm("Would you like lowercase characters?");
+        var upper = confirm("Would you like uppercase characters?");
+        var special = confirm("Would you like special characters?");
+        var numbers = confirm("Would you like numerical characters?");
+    }
 }
